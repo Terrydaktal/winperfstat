6a9736a0b74e5d23ad237e0f6a33e717e349e907
Terrydaktal <9lewis9@gmail.com>
2021-04-16 05:16:09 +0100

    Create driver.c

--- driver.c
+++ driver.c
@@ -7,19 +7,19 @@ NTSTATUS DriverEntry(IN PDRIVER_OBJECT DriverObject, IN PUNICODE_STRING Registry
 NTSTATUS NotImplementedDispatch(IN PDEVICE_OBJECT DeviceObject, IN PIRP Irp);
 NTSTATUS IoCtlDispatch(IN PDEVICE_OBJECT DeviceObject, IN PIRP Irp);
 VOID UnloadHandler(IN PDRIVER_OBJECT DriverObject);
-NTSTATUS CreateCloseDispatch(IN PDEVICE_OBJECT DeviceObject, IN PIRP Irp);
 VOID MeasureApp(IN char** inputBuffer);
+NTSTATUS CreateCloseDispatch(IN PDEVICE_OBJECT DeviceObject, IN PIRP Irp);
 
 #ifdef ALLOC_PRAGMA
 #pragma alloc_text (INIT, DriverEntry)
 #pragma alloc_text (PAGE, NotImplementedDispatch)
 #pragma alloc_text (PAGE, IoCtlDispatch)
 #pragma alloc_text (PAGE, UnloadHandler)
-#pragma alloc_text (PAGE, CreateCloseDispatch)
-#pragma alloc_text (PAGELK, MeasureApp)
+#pragma alloc_text (PAGE, MeasureApp)
+#pragma alloc_text (PAGELK, CreateCloseDispatch)
 #endif
 
-#define BENCHMARK_DRV_IOCTL 0x69
+#define BENCHMARK_DRV_IOCTL CTL_CODE(FILE_DEVICE_UNKNOWN, 0x902, METHOD_NEITHER, FILE_ANY_ACCESS)
 #define IA32_PERF_EVTSEL(x) 0x186+x
 #define BuildEvent(EVT, UMASK, USR, OS, E, PC, INTT, ANY, EN, INV, CMASK) (EVT + (UMASK << 8) + (USR << 16) + (OS << 17) + (E << 18) + (PC << 19) + (INTT << 20) + (ANY << 21) + (EN << 22) + (INV << 23) + (CMASK << 24))
 
@@ -92,7 +92,19 @@ NTSTATUS DriverEntry(
 	NTSTATUS Status = STATUS_UNSUCCESSFUL;
 	UNICODE_STRING DeviceName, DosDeviceName = { 0 };
 	RtlInitUnicodeString(&DeviceName, L"\\Device\\winperfstat");
-	RtlInitUnicodeString(&DosDeviceName, L"\\DosDevices\\winperfstat"); // in Global Name: winperfstat SymLink \Device\winperfstat
+	RtlInitUnicodeString(&DosDeviceName, L"\\??\\winperfstat"); // in Global Name: winperfstat SymLink \Device\winperfstat
+
+	for (int i = 0; i <= IRP_MJ_MAXIMUM_FUNCTION; i++) {
+		#pragma warning(push) // Disable the Compiler Warning: 28169
+		#pragma warning(disable : 28169) 
+		DriverObject->MajorFunction[i] = NotImplementedDispatch;
+		#pragma warning(pop)
+	}
+
+	DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = IoCtlDispatch;
+	DriverObject->MajorFunction[IRP_MJ_CREATE] = CreateCloseDispatch;
+	DriverObject->MajorFunction[IRP_MJ_CLOSE] = CreateCloseDispatch;
+	DriverObject->DriverUnload = UnloadHandler;
 
 	Status = IoCreateDevice(DriverObject,
 		0,
@@ -107,25 +119,16 @@ NTSTATUS DriverEntry(
 			IoDeleteDevice(DeviceObject);
 		}
 
+		KeBugCheck(1);
 		return Status;
 	}
 
-	for (int i = 0; i <= IRP_MJ_MAXIMUM_FUNCTION; i++) {
-		#pragma warning(push) // Disable the Compiler Warning: 28169
-		#pragma warning(disable : 28169) 
-		DriverObject->MajorFunction[i] = NotImplementedDispatch;
-		#pragma warning(pop)
-	}
-
-	DriverObject->MajorFunction[IRP_MJ_CREATE] = CreateCloseDispatch;
-	DriverObject->MajorFunction[IRP_MJ_CLOSE] = CreateCloseDispatch;
-	DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = IoCtlDispatch;
-	DriverObject->DriverUnload = UnloadHandler;
+	Status = IoCreateSymbolicLink(&DosDeviceName, &DeviceName);
+	if (Status != STATUS_SUCCESS) KeBugCheck(2);
 
 	DeviceObject->Flags |= DO_DIRECT_IO;
 	DeviceObject->Flags &= ~DO_DEVICE_INITIALIZING;
-
-	Status = IoCreateSymbolicLink(&DosDeviceName, &DeviceName);
+	
 	return Status;
 }
 
@@ -134,9 +137,9 @@ VOID UnloadHandler(
 	IN PDRIVER_OBJECT DriverObject
 )
 {
-	UNICODE_STRING DosDeviceName = { 0 };
-
 	PAGED_CODE();
+
+	UNICODE_STRING DosDeviceName = { 0 };
 
 	RtlInitUnicodeString(&DosDeviceName, L"\\DosDevices\\winperfstat");
 
@@ -146,33 +149,16 @@ VOID UnloadHandler(
 	// Driver Unloaded
 }
 
-NTSTATUS NotImplementedDispatch(
-	IN PDEVICE_OBJECT DeviceObject,
-	IN PIRP Irp
-)
-{
-	Irp->IoStatus.Information = 0;
-	Irp->IoStatus.Status = STATUS_NOT_SUPPORTED;
-
-	UNREFERENCED_PARAMETER(DeviceObject);
-	PAGED_CODE();
-
-	// Complete the request
-	IoCompleteRequest(Irp, IO_NO_INCREMENT);
-
-	return STATUS_NOT_SUPPORTED;
-}
-
 NTSTATUS CreateCloseDispatch(
 	IN PDEVICE_OBJECT DeviceObject,
 	IN PIRP Irp
 )
 {
-	Irp->IoStatus.Information = 0;
-	Irp->IoStatus.Status = STATUS_SUCCESS;
-
 	UNREFERENCED_PARAMETER(DeviceObject);
 	PAGED_CODE();
+
+	Irp->IoStatus.Information = 0;
+	Irp->IoStatus.Status = STATUS_SUCCESS;
 
 	// Complete the request
 	IoCompleteRequest(Irp, IO_NO_INCREMENT);
@@ -180,27 +166,51 @@ NTSTATUS CreateCloseDispatch(
 	return STATUS_SUCCESS;
 }
 
+NTSTATUS NotImplementedDispatch(
+	IN PDEVICE_OBJECT DeviceObject,
+	IN PIRP Irp
+)
+{
+	UNREFERENCED_PARAMETER(DeviceObject);
+	PAGED_CODE();
+
+	Irp->IoStatus.Information = 0;
+	Irp->IoStatus.Status = STATUS_NOT_SUPPORTED;
+
+	// Complete the request
+	IoCompleteRequest(Irp, IO_NO_INCREMENT);
+
+	return STATUS_NOT_SUPPORTED;
+}
+
 NTSTATUS IoCtlDispatch(
 	IN PDEVICE_OBJECT DeviceObject,
 	IN PIRP Irp
 )
 {
-	PIO_STACK_LOCATION IrpSp = NULL;
-	NTSTATUS Status = STATUS_NOT_SUPPORTED;
-	ULONG IoControlCode = IrpSp->Parameters.DeviceIoControl.IoControlCode; // no minor code only io control code
-	char** inputBuffer;
-
-	HANDLE hThread;
-	PETHREAD ThreadObject;
 	UNREFERENCED_PARAMETER(DeviceObject);
 	PAGED_CODE();
 
+	PIO_STACK_LOCATION IrpSp = IoGetCurrentIrpStackLocation(Irp);
+	ULONG IoControlCode = IrpSp->Parameters.DeviceIoControl.IoControlCode; // no minor code only io control code
+	NTSTATUS Status = STATUS_NOT_SUPPORTED;
+	unsigned long long * inputBuffer;
+	int inputBufferlen;
+	HANDLE hThread;
+	PETHREAD ThreadObject;
 	HANDLE hProcess = ZwCurrentProcess();
 
-	IrpSp = IoGetCurrentIrpStackLocation(Irp);
 	inputBuffer = IrpSp->Parameters.DeviceIoControl.Type3InputBuffer;
-	*((PIRP*)IrpSp->Parameters.DeviceIoControl.Type3InputBuffer + 1) = Irp;
-
+	inputBufferlen = IrpSp->Parameters.DeviceIoControl.InputBufferLength;
+	
+	__try {
+		ProbeForWrite(inputBuffer, inputBufferlen, 1);
+		inputBuffer[1] = (unsigned long long) Irp;
+	} 
+	__except(EXCEPTION_EXECUTE_HANDLER)
+	{
+		goto fail;
+	}
 
 	if (IrpSp) {
 		switch (IoControlCode) {
@@ -248,6 +258,7 @@ NTSTATUS IoCtlDispatch(
 			break;
 
 		default:
+			fail:
 			Status = STATUS_INVALID_DEVICE_REQUEST;
 			break;
 		}
@@ -278,17 +289,24 @@ void MeasureApp(
 	numParams = inputBufferlen / sizeof(PVOID);
 	PULONGLONG MSRBuffer = ExAllocatePool(NonPagedPool, inputBufferlen);
 	PULONGLONG CountBuffer = ExAllocatePool(NonPagedPool, inputBufferlen);
-
-	oldIrql = KeRaiseIrqlToSynchLevel();
+	ANSI_STRING aevstr, ainstr;
+	UNICODE_STRING evstr, instr;
 
 	for (int j = 2; j < numParams; j++) {
+		RtlInitAnsiString(&ainstr, inputBuffer[j]);
+		RtlAnsiStringToUnicodeString(&instr, &ainstr, TRUE);
+		
 		for (int i = 0; i < sizeof(Events) / sizeof(ULONGLONG); i++) {
-			if (strcmp(Events[i], inputBuffer[j]))
+			RtlInitAnsiString(&aevstr, Events[i]);
+			RtlAnsiStringToUnicodeString(&evstr, &aevstr, TRUE);
+			if (RtlEqualUnicodeString(&evstr, &instr, OBJ_CASE_INSENSITIVE))
 			{
 				MSRBuffer[j] = EventMSRValues[i];
 			}
 		}
 	}
+
+	oldIrql = KeRaiseIrqlToSynchLevel();
 
 	for (int i = 2; i < numParams; i++) {
 		if (MSRBuffer[i]) {
@@ -309,6 +327,11 @@ void MeasureApp(
 	KeLowerIrql(oldIrql);
 
 	MmUnlockPagableImageSection(hSection);
+	
+	CountBuffer[2] = __readpmc(0);
+
+	CountBuffer[3] = 3;
+
 	RtlCopyMemory(Irp->UserBuffer, CountBuffer, inputBufferlen);
 	ExFreePool(MSRBuffer);
 	ExFreePool(CountBuffer);
